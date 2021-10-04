0bf2d5e3c8d13591b7bc7720e3d32f48afa40a60
ksemanur <59751315+ksemanur8@users.noreply.github.com>
2021-10-04 18:57:52 -0400

    Using FullPage

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+	});
+	//methods
+	$.fn.fullpage.setAllowScrolling(false);
+});
